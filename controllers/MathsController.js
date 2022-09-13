cfb658d7ac2ea7a9dda36cd86744da3fa098e5eb
Jeremy Labelle <jgoodgoal@hotmail.ca>
2022-09-13 14:04:24 -0400

    Modification général et complète

--- controllers/MathsController.js
+++ controllers/MathsController.js
@@ -8,111 +8,179 @@ module.exports =
         constructor(HttpContext) {
             super(HttpContext);
         }
+        /*checkParamsCount(nbParams){
+            if(Object.keys(this.params).length > nbParams){
+                return this.error("too many parameters");
+            }
+            return true;
+        }*/
         get() {
-            if(this.HttpContext.path.queryString == '?'){
+            const params = this.HttpContext.path.params;
+            if (this.HttpContext.path.queryString == '?') {
                 // Send html help page
                 let helpPagePath = path.join(process.cwd(), "wwwroot/helpPages/mathServiceHelp.html");
                 let content = fs.readFileSync(helpPagePath);
                 this.HttpContext.response.content("text/html", content);
-            }else{
-                if(this.HttpContext.path.params.op){
-                    switch(this.HttpContext.path.params.op){
-                        case('+'):
-                        this.HttpContext.path.params.op = '+';
-                        x = this.HttpContext.path.params.x;
-                        y = this.HttpContext.path.params.y;
-                        this.HttpContext.path.params.value = parseInt(x) + parseInt(y);
-                        this.HttpContext.response.JSON(this.HttpContext.path.params);
-                        break;
+            } else {
+                if (params.op) {
+                    switch (params.op) {
+                        case (' '):
+                            if (Object.keys(params).length > 3) {
+                                params.error = "too many params";
+                                return this.HttpContext.response.JSON(params);
+                            }
+                            if(!params.x || !params.y){
+                                params.error = "params missing";
+                                return this.HttpContext.response.JSON(params);
+                            }
+                            params.op = '+';
+                            x = parseInt(params.x);
+                            y = parseInt(params.y);
+                            params.value = x + y;
+                            this.HttpContext.response.JSON(params);
+                            break;
 
-                        case(' '):
-                        this.HttpContext.path.params.op = '+';
-                        x = this.HttpContext.path.params.x;
-                        y = this.HttpContext.path.params.y;
-                        this.HttpContext.path.params.value = parseInt(x) + parseInt(y);
-                        this.HttpContext.response.JSON(this.HttpContext.path.params);
-                        break;
+                        case ('-'):
+                            if (Object.keys(params).length > 3) {
+                                params.error = "too many params";
+                                return this.HttpContext.response.JSON(params);
+                            }
+                            if(!params.x || !params.y){
+                                params.error = "params missing";
+                                return this.HttpContext.response.JSON(params);
+                            }
+                            x = parseInt(params.x);
+                            y = parseInt(params.y);
+                            params.value = x - y;
+                            this.HttpContext.response.JSON(params);
+                            break;
 
-                        case('-'):
-                        x = this.HttpContext.path.params.x;
-                        y = this.HttpContext.path.params.y;
-                        this.HttpContext.path.params.value = parseInt(x) - parseInt(y);
-                        this.HttpContext.response.JSON(this.HttpContext.path.params);
-                        break;
+                        case ('*'):
+                            if (Object.keys(params).length > 3) {
+                                params.error = "too many params";
+                                return this.HttpContext.response.JSON(params);
+                            }
+                            if(!params.x || !params.y){
+                                params.error = "params missing";
+                                return this.HttpContext.response.JSON(params);
+                            }
+                            x = parseInt(params.x);
+                            y = parseInt(params.y);
+                            params.value = x * y;
+                            this.HttpContext.response.JSON(params);
+                            break;
 
-                        case('*'):
-                        x = this.HttpContext.path.params.x;
-                        y = this.HttpContext.path.params.y;
-                        this.HttpContext.path.params.value = parseInt(x) * parseInt(y);
-                        this.HttpContext.response.JSON(this.HttpContext.path.params);
-                        break;
+                        case ('/'):
+                            if (Object.keys(params).length > 3) {
+                                params.error = "too many params";
+                                return this.HttpContext.response.JSON(params);
+                            }
+                            if(!params.x || !params.y){
+                                params.error = "params missing";
+                                return this.HttpContext.response.JSON(params);
+                            }
+                            x = parseInt(params.x);
+                            y = parseInt(params.y);
+                            if (y === 0) {
+                                params.error = "Impossible de diviser un nombre par 0";
+                            }
+                            params.value = x / y;
+                            this.HttpContext.response.JSON(params);
+                            break;
 
-                        case('/'):
-                        x = parseInt(this.HttpContext.path.params.x);
-                        y = parseInt(this.HttpContext.path.params.y);
-                        if(y === 0){
-                            this.HttpContext.path.params.error = "Impossible de diviser un nombre par 0";
-                        }
-                        this.HttpContext.path.params.value = x / y;
-                        this.HttpContext.response.JSON(this.HttpContext.path.params);
-                        break;
+                        case ('%'):
+                            if (Object.keys(params).length > 3) {
+                                params.error = "too many params";
+                                return this.HttpContext.response.JSON(params);
+                            }
+                            if(!params.x || !params.y){
+                                params.error = "params missing";
+                                return this.HttpContext.response.JSON(params);
+                            }
+                            x = parseInt(params.x);
+                            y = parseInt(params.y);
+                            if (y === 0) {
+                                params.error = "Impossible de moduler un nombre par 0";
+                            }
+                            params.value = x % y;
+                            this.HttpContext.response.JSON(params);
+                            break;
 
-                        case('%'):
-                        x = parseInt(this.HttpContext.path.params.x);
-                        y = parseInt(this.HttpContext.path.params.y);
-                        if(y === 0){
-                            this.HttpContext.path.params.error = "Impossible de moduler un nombre par 0";
-                        }
-                        this.HttpContext.path.params.value = x % y;
-                        this.HttpContext.response.JSON(this.HttpContext.path.params);
-                        break;
+                        case ('!'):
+                            if (Object.keys(params).length > 2) {
+                                params.error = "too many params";
+                                return this.HttpContext.response.JSON(params);
+                            }
+                            if(!params.n){
+                                params.error = "params missing";
+                                return this.HttpContext.response.JSON(params);
+                            }
+                            n = params.n;
+                            if (n < 0) {
+                                params.error = "Impossible de calculer la factorielle d'un nombre négatif";
+                                n = 0;
+                            }
+                            let facto = 1;
+                            for (var i = 1; i <= n; ++i) {
+                                facto = facto * i;
+                            }
+                            params.value = facto;
+                            this.HttpContext.response.JSON(params);
+                            break;
 
-                        case('!'):
-                        n = this.HttpContext.path.params.n;
-                        let facto = 1;
-                        for(var i = 1; i <= n; ++i){
-                            facto = facto * i;
-                        }
-                        this.HttpContext.path.params.value = facto;
-                        this.HttpContext.response.JSON(this.HttpContext.path.params);
-                        break;
+                        case ('p'):
+                            if (Object.keys(params).length > 2) {
+                                params.error = "too many params";
+                                return this.HttpContext.response.JSON(params);
+                            }
+                            if(!params.n){
+                                params.error = "params missing";
+                                return this.HttpContext.response.JSON(params);
+                            }
+                            n = params.n;
+                            params.value = isPrime(n);
+                            this.HttpContext.response.JSON(params);
+                            break;
 
-                        case('p'):
-                        n = this.HttpContext.path.params.n;
-                        this.HttpContext.path.params.value = isPrime(n);
-                        this.HttpContext.response.JSON(this.HttpContext.path.params);
-                        break;
-
-                        case('np'):// qu'est-ce ue cette fonction envoie?
-                        n = this.HttpContext.path.params.n;
-                        this.HttpContext.path.params.value = findPrime(n);
-                        this.HttpContext.response.JSON(this.HttpContext.path.params);
-                        break;
+                        case ('np'):
+                            if (Object.keys(params).length > 2) {
+                                params.error = "too many params";
+                                return this.HttpContext.response.JSON(params);
+                            }
+                            if(!params.n){
+                                params.error = "params missing";
+                                return this.HttpContext.response.JSON(params);
+                            }
+                            n = params.n;
+                            params.value = findPrime(n);
+                            this.HttpContext.response.JSON(params);
+                            break;
                     }
                 }
-                else{
-                    this.HttpContext.path.params.error = "parameter 'op' is missing";
-                    this.HttpContext.response.JSON(this.HttpContext.path.params);
+                else {
+                    params.error = "parameter 'op' is missing";
+                    this.HttpContext.response.JSON(params);
+
                 }
             }
         }
     }
 
-    function isPrime(value) {
-        for(var i = 2; i < value; i++) {
-            if(value % i === 0) {
-                return false;
-            }
+function isPrime(value) {
+    for (var i = 2; i < value; i++) {
+        if (value % i === 0) {
+            return false;
         }
-        return value > 1;
     }
-    function findPrime(n){
-        let primeNumer = 0;
-        for ( let i=0; i < n; i++){
+    return value > 1;
+}
+function findPrime(n) {
+    let primeNumer = 0;
+    for (let i = 0; i < n; i++) {
+        primeNumer++;
+        while (!isPrime(primeNumer)) {
             primeNumer++;
-            while (!isPrime(primeNumer)){
-                primeNumer++;
-            }
         }
-        return primeNumer;
     }
+    return primeNumer;
+}
